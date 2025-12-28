87f8e9ef83a4126e2711b140fc323ffd02490754
annacsm77 <annacsm77@gmail.com>
2025-12-28 12:39:48 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestione Apiario</title>
+       <link rel="stylesheet" href="<?php echo TPL_URL; ?>styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+<body>
+    <header>
+        <h1>Gestione Apiario</h1>
+    <?php include BASE_PATH . 'includes/menu.php'; ?>
+    </header>
+   
